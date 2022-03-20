7b0d535b6fd38dcba4830453c24d4eacf7bcb594
worawut <jamesworawut1996@gmail.com>
2022-03-21 01:10:08 +0700

    add router

--- main.go
+++ main.go
@@ -2,9 +2,7 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
-	"os"
 
 	"github.com/gin-gonic/gin"
 )
@@ -23,6 +21,7 @@ var albums = []ablum{
 }
 
 func getAlbums(c *gin.Context) {
+	fmt.Println(albums)
 	c.IndentedJSON(http.StatusOK, albums)
 }
 
@@ -83,36 +82,18 @@ func deleteAlbumById(c *gin.Context) {
 	c.JSON(http.StatusNotFound, "data not found")
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	name := os.Getenv("NAME")
-	if name == "" {
-		name = "World"
-	}
-	fmt.Fprintf(w, "Hello %s!\n", name)
+func getHome(c *gin.Context) {
+	c.HTML(http.StatusOK, "index.html", nil)
 }
 
 func main() {
-	log.Print("starting server...")
-	http.HandleFunc("/", handler)
-
-	// Determine port for HTTP service.
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-		log.Printf("defaulting to port %s", port)
-	}
-
-	// Start HTTP server.
-	log.Printf("listening on port %s", port)
-	if err := http.ListenAndServe(":"+port, nil); err != nil {
-		log.Fatal(err)
-	}
-
 	router := gin.Default()
 	router.LoadHTMLGlob("page/*.html")
+	router.GET("/", getHome)
 	router.GET("/albums", getAlbums)
 	router.GET("/albums/:id", getAlbumById)
 	router.POST("/albums", postAlbums)
 	router.PUT("/albums/:id", updateAlbums)
 	router.DELETE("/albums/:id", deleteAlbumById)
+	router.Run()
 }
